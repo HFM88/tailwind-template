1012f7338164b3e5e426fd208ccc248bd9f692ad
DarkMole <gameandanimation2@gmail.com>
2024-04-20 00:06:36 +0300

    Co-authored-by: gibu2520 <gibu2520@users.noreply.github.com> Co-authored-by: ionutbruh <ionutbruh@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
       },
       colors: {
-        primary: "#b72027",
+        primarygray: "#212121",
+        accent: "#405cf5",
+        background: "#141414",
+        alice: "#f0f8ff",
         secondary: "#222224",
         text: "",
         subtext: "#343A40",
@@ -20,7 +23,25 @@ module.exports = {
         navpx: "40px",
         navpy: "20px",
       },
+      backgroundImage: {
+        "logo": "url('path/to/logo')",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const glassStyles = {
+        ".glass": {
+          background:
+            "linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0))",
+          "backdrop-filter": "blur(10px)",
+          "-webkit-backdrop-filter": "blur(10px)",
+          border: "1px solid rgba(255, 255, 255, 0.18)",
+          "box-shadow": "0 8px 32px 0 rgba(0, 0, 0, 0.37)",
+        },
+      };
+
+      addUtilities(glassStyles, ["responsive", "hover"]);
+    },
+  ],
 };
